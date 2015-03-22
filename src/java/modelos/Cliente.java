8c6993a9eee5dc5cf38f86c882f7496f2a58702f
Jefferson Medeiros <nobertojefferson78@gmail.com>
2015-03-22 20:47:15 -0300

    Versao 1
    desenvolvendo as classes cliente e pessoa

--- src/java/modelos/Cliente.java
+++ src/java/modelos/Cliente.java
@@ -1,35 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package modelos;
 
-import java.util.List;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-public class Cliente {
-	
-	private long id;
-	private Pessoa pessoa;
-	private List<Animal> animais;
-	
-	public Cliente(){
-		
-	}
+/**
+ *
+ * @author jefferson
+ */
+@Entity
+public class Cliente implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-	public long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
 
-	public List<Animal> getAnimais() {
-		return animais;
-	}
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
 
-	public void setAnimais(List<Animal> animais) {
-		this.animais = animais;
-	}
-	
-	
-
+    @Override
+    public String toString() {
+        return "modelos.Cliente[ id=" + id + " ]";
+    }
+    
 }
--- src/java/modelos/Pessoa.java
+++ src/java/modelos/Pessoa.java
@@ -5,22 +5,36 @@
  */
 package modelos;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 /**
  *
  * @author jefferson
  */
-public class Pessoa {
+@Entity
+public class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String nome;
     private String cpf;
+    @OneToOne
     private Endereco endereco;
 
-    /**
-     * @return the nome
-     */
-    public String getNome() {
-        return nome;
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
     /**
      * @param nome the nome to set
      */
@@ -55,6 +69,39 @@ public class Pessoa {
     public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pessoa)) {
+            return false;
+        }
+        Pessoa other = (Pessoa) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelos.Pessoa[ id=" + id + "nome=" + nome + "cpf=" + cpf + " ]";
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
     
     
 }
